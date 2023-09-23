be326383b6a4157e0a2866fa18697222eaab88cb
Homa76 <118890890+76homa@users.noreply.github.com>
2023-09-23 09:16:59 +0330

    Update 24 - 4G Sector Trend database-Daily Report_ 16 Mordad - 15 Shahrivar-new.py

--- 24 - 4G Sector Trend database-Daily Report_ 16 Mordad - 15 Shahrivar-new.py
+++ 24 - 4G Sector Trend database-Daily Report_ 16 Mordad - 15 Shahrivar-new.py
@@ -1,50 +1,46 @@
-import pandas as pd
-import dask.dataframe as dd
 import numpy as np
-from dask.distributed import Client
-from sklearn.preprocessing import LabelBinarizer
+import matplotlib.pyplot as plt
+import pandas as pd
 
-def mycal():
-    # Create a Dask client (optional but recommended)
-    client = Client()
-    
-    # Read data using pandas
-    excel_file_path = 'C:/Users/homa.behmardi/Downloads/24 - 4G Sector Trend database-Daily Report_ 16 Mordad - 15 Shahrivar-new.xlsx'
-    sheet_name = 'Report'  # Replace with the correct sheet name
-    
-    pandas_data = pd.read_excel(excel_file_path, sheet_name=sheet_name)
-    print('Read excel Completed....')
-    # Convert the pandas DataFrame to a Dask DataFrame
-    data = dd.from_pandas(pandas_data, npartitions=10)  # You can adjust the number of partitions as needed
-    print('Data Partitioned...')
-    # Specify the categorical column
-    categorical_columns = ['Sector']
-    
-    # Apply LabelBinarizer in parallel
-    label_binarizer = LabelBinarizer()
-    label_encoded = data[categorical_columns].map_partitions(label_binarizer.fit_transform, meta=data[categorical_columns])
-    
-    print('l5')
-    #print('Lable Encode Shape : ', np.shape(label_encoded))
-    # Compute the result (this triggers the computation)
-    label_encoded = label_encoded.compute()
-    print('Lable Computing finished...')
-    
-    # Get the feature names (unique classes)
-    feature_names = label_binarizer.classes_
-    
-    # Create a DataFrame with the encoded data
-    label_encoded_df = pd.DataFrame(label_encoded, columns=feature_names)
-    
-    # Concatenate the encoded data with the original data
-    data_encoded = pd.concat([pandas_data, label_encoded_df], axis=1)
-    
-    # Optionally, drop the original categorical column
-    data_encoded.drop(categorical_columns, axis=1, inplace=True)
-    
-    # Optionally, save the encoded data to a CSV file
-    data_encoded.to_csv('encoded_data.csv', index=False)
+# Sample data
+file_path = "C:/Users/homa.behmardi/Downloads/24 - 4G Sector Trend database-Daily Report_ 16 Mordad - 15 Shahrivar.xlsx"
+data = pd.read_excel(file_path, sheet_name="Report")
 
+# Extract the relevant columns from your DataFrame
+connected_users_of_days = data["connected users of days"]
+throughput = data["Average  DL Thr Mbps"]
+average_traffic_gb = data["Average Traffic (GB)"]
+average_dl_prb = data["Average DL PRB"]
 
-if __name__ == '__main__':
-    mycal()
+# Define traffic clusters based on specific ranges
+conditions = [
+    (average_traffic_gb < 100),
+    (average_traffic_gb >= 100) & (average_traffic_gb < 600),
+    (average_traffic_gb >= 600)
+]
+
+cluster_labels = np.select(conditions, ["< 100", "100 < x < 600", "> 600"])
+
+# Define different colors for each traffic cluster
+colors = {'< 100': 'red', '100 < x < 600': 'blue', '> 600': 'green'}
+
+# Create a scatter plot with different colors for each traffic cluster and shapes based on DL PRB
+for cluster_label in np.unique(cluster_labels):
+    mask_cluster = cluster_labels == cluster_label
+    plt.scatter(
+        connected_users_of_days[mask_cluster],
+        throughput[mask_cluster],
+        c=colors[cluster_label],  # Assign a different color for each traffic cluster
+        marker='o',  # Use the same marker for all points
+        label=f'Traffic Cluster: {cluster_label}',
+    )
+
+# Set labels and legend
+plt.xlabel('Connected Users of Days')
+plt.ylabel('Average DL Thr Mbps')
+plt.legend()
+
+# Show the plot
+plt.grid(True)
+plt.title('Scatter Plot with Different Colors Based on Traffic Clusters')
+plt.show()
